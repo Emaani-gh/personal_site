ee05dfb5306e0a98d4c53a6c711813c296fd62e7
Emaani-gh <abdulsalamseidu@protonmail.com>
2024-03-21 22:29:41 +0200

    nav and uncomplete services section

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,192 @@
+"use client";
+
+import { useState } from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillInstagram, AiFillGithub } from "react-icons/ai";
+
 import Image from "next/image";
+import Seidu from "/public/seidu.png";
+import websiteIcon from "/public/web-icon.png";
+import webApp from "/public/web-icon.png";
 
 export default function Home() {
+  const [openMenu, setOpenmenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenmenu((prev) => !prev);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="px-6 md:container md:mx-auto font-poppins ">
+      <nav className=" flex justify-between items-center py-6 relative">
+        {/* ---------LOGO----------------- */}
+        <div className="logo">
+          <h2 className=" font-prism font-bold text-2xl cursor-pointer">
+            SEIDU
+          </h2>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="flex items-center gap-6">
+          <BsFillMoonStarsFill className=" cursor-pointer text-lg" />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {/* ==================================MOBILE MENUS======================================= */}
+          <ul
+            className={`menus flex flex-col items-center w-full gap-6 absolute 
+            left-0 top-20 md:hidden ${openMenu ? "openMobile" : ""}`}
+          >
+            <li>
+              <a className="text-black text-xl" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="text-black text-xl" href="#">
+                Projects
+              </a>
+            </li>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <li>
+              <a className="text-black text-xl" href="#">
+                Blogs
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-xl bg-cyan-500 px-4 py-2 rounded-md text-white"
+                href="#"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+          {/* ==================================DESK MENUS======================================= */}
+          <ul
+            className={`hidden md:flex gap-4  ${openMenu ? "openMobile" : ""}`}
+          >
+            <li>
+              <a className="text-black text-xl" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="text-black text-xl" href="#">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="text-black text-xl" href="#">
+                Blogs
+              </a>
+            </li>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <li>
+              <a
+                className="text-xl bg-cyan-500 px-4 py-2 rounded-md text-white"
+                href="#"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+          {/* ==================================HAMBURGER======================================= */}
+          <div
+            className={`hamburger w-8 md:hidden cursor-pointer ${
+              openMenu ? "showMobile" : ""
+            }`}
+            onClick={toggleMenu}
+          >
+            <div className="line-1 w-full h-[3px] bg-black "></div>
+            <div className="line-2 w-full h-[3px] bg-black my-1"></div>
+            <div className="line-3 w-full h-[3px] bg-black "></div>
+          </div>
+        </div>
+      </nav>
+      {/* ==================================END OF NAV======================================= */}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* ==================================HERO SECTION======================================= */}
+
+      <section id="hero" className=" mt-16">
+        <div className="md:flex  justify-between gap-4 max-w-6xl mx-auto md:shadow-md md:p-8">
+          <div className="flex flex-col items-center md:items-start ">
+            <h2 className="text-teal-600 text-4xl font-bold py-2 text-center">
+              Seidu Abdul Salam
+            </h2>
+            <h4 className=" py-2 text-gray-500 font-semibold text-xl">
+              Web Developer
+            </h4>
+            <p className="max-w-lg text-center md:text-left text-gray-500 text-lg py-4 font-medium">
+              Full Stack developer with passion for creating new things. I love
+              to learn new skills and technology and also enjoy football.
+            </p>
+            <div className="social-icons flex justify-center gap-10 py-5 text-4xl text-gray-500">
+              <a href="#">
+                <AiFillLinkedin className=" cursor-pointer" />
+              </a>
+              <a href="#">
+                <AiFillGithub className=" cursor-pointer" />
+              </a>
+              <a href="#">
+                <AiFillInstagram className=" cursor-pointer" />
+              </a>
+            </div>
+          </div>
+
+          <div className="relative flex w-72 h-72 p-4 rounded-full bg-gradient-to-b from-teal-500 mt-16 overflow-hidden mx-auto md:mt-0 ">
+            <Image src={Seidu} layout="fill" objectFit="cover" />
+          </div>
+        </div>
+      </section>
+
+      {/* =========================SECTION SERVICES========================= */}
+      <section id="service" className="my-24">
+        <h2 className="py-8 text-2xl text-gray-600 font-medium text-center">
+          What I Do
+        </h2>
+        <div className="cards">
+          <div className="flex flex-col items-center shadow-md p-8 rounded-lg mb-10">
+            <Image src={websiteIcon} className="py-6" />
+            <h3 className="text-2xl text-gray-600 font-medium py-4">
+              Frontend Development
+            </h3>
+            <p className="text-gray-600 text-lg font-medium py-4">
+              I build responsive and interactive websites using modern
+              technologies
+            </p>
+            <h4 className="text-lg text-teal-500 font-bold py-2">
+              Skills & Technologies
+            </h4>
+            <ul className="text-center">
+              <li className=" text-gray-700 font-medium py-1">HTML</li>
+              <li className=" text-gray-700 font-medium py-1">
+                CSS, Tailwind, Bootstrap
+              </li>
+              <li className=" text-gray-700 font-medium py-1">JavaScript</li>
+              <li className=" text-gray-700 font-medium py-1">React</li>
+            </ul>
+          </div>
+
+          {/* <div className="flex flex-col items-center shadow-md p-8 rounded-lg ">
+            <Image src={websiteIcon} className="py-6" />
+            <h3 className="text-2xl text-gray-600 font-medium py-4">
+              Frontend Development
+            </h3>
+            <p className="text-gray-600 text-lg font-medium py-4">
+              I build responsive and interactive websites using modern
+              technologies
+            </p>
+            <h4 className="text-lg text-teal-500 font-bold py-2">
+              Skills & Technologies
+            </h4>
+            <ul className="text-center">
+              <li className=" text-gray-700 font-medium py-1">HTML</li>
+              <li className=" text-gray-700 font-medium py-1">
+                CSS, Tailwind, Bootstrap
+              </li>
+              <li className=" text-gray-700 font-medium py-1">JavaScript</li>
+              <li className=" text-gray-700 font-medium py-1">React</li>
+            </ul>
+          </div> */}
+        </div>
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,11 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: {},
+    fontFamily: {
+      prism: ["Tilt Prism", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
     },
   },
   plugins: [],
